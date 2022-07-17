ba596df246f27e8662408c9eb0d6115db91d3ef5
powcoder代写 <powcoder@163.com>
2022-07-17 16:04:49 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ Simulation.h
@@ -0,0 +1,40 @@
+
+// header files
+#include "StringUtils.h"
+#include "MetaDataAccess.h"
+
+// create global constants - across files
+typedef enum { NEW,
+               READY,
+               RUNNING,
+               WAITING,
+               TERMINATED } CurrentState;
+
+// config data structure
+typedef struct ProcessCtrlBlock
+   {
+    int state;
+    int totalRumTime;
+    int countNumber;
+    
+    struct ProcessCtrlBlock *next;
+   }ProcessCtrlBlock;
+
+typedef struct StoreOutputString
+   {
+    char outputString[ 100 ];
+    
+    struct StoreOutputString *next;
+   } StoreOutputString;
+
+// function prototypes
+void displaySimulation( OpCodeType *metaDataOpPtr, ConfigDataType *configDataPtr, int controlCode);
+double addProcessTime( OpCodeType *localPtr, ConfigDataType *configData );
+StoreOutputString *outPutManager( char *outputBuffer, int controlCode, StoreOutputString *localHeader );
+StoreOutputString *addOutputStringNode( StoreOutputString *localPtr, StoreOutputString *newNode, char *outputBuffer );
+void outputToFile( StoreOutputString *localHeader, ConfigDataType *configData );
+void *threadRunTimer( void *time );
+ProcessCtrlBlock *createPCBList( ProcessCtrlBlock *processHeader, OpCodeType *localPtr );
+ProcessCtrlBlock *addPCBNode( ProcessCtrlBlock *localPtr, ProcessCtrlBlock *newNode );
+ProcessCtrlBlock *getReadyState( ProcessCtrlBlock *processHeader );
+
